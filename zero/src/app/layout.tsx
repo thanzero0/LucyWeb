a9fd97b6e841ab4e174b956605d2d462700fd314
Sulthan <thanzero7@gmail.com>
2026-01-23 11:08:51 +0700

    edit layout

--- zero/src/app/layout.tsx
+++ zero/src/app/layout.tsx
@@ -4,3 +4,17 @@ export const metadata = {
   title: 'Lucy // Cyberpunk',
   description: 'Minimal cyberpunk themed Next.js app',
 }
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
